4da1f3fc0c2bfeaa9bc73275ca29033534d20ff9
fullyslick <default@mail.com>
2018-07-03 16:46:05 +0300

    Add the data the app will use

--- src/App.js
+++ src/App.js
@@ -7,6 +7,29 @@
 
 import React, {Component} from 'react';
 
+// The data that will be using,
+// later it will be replaced by fetch to server localhost:5001
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
+
 class App extends Component {
   render() {
     // Starting state of the app
