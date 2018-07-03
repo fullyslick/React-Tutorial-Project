1a2029278f64813630d935ee04d2e07f222769d6
fullyslick <default@mail.com>
2018-07-03 16:02:05 +0300

    How to use properties of components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 
+class ContactList extends React.Component{
+  render(){
+
+  // Get the data from the "contacts" property of the <ContactList />
+  const people = this.props.contacts;
+
+  // Create an <ul> and then <li> for every contact
+  return (<ul>
+    { people.map(
+      person => (<li key={person.name}>{person.name}</li>)
+     )}
+    </ul>);
+ }
+}
+
+// Here the App component returns the ContactList component
+// Check this video: NOTE https://youtu.be/PtospOO4qrs
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={[
+          {name: 'Derrick'},
+          {name: 'Scott'},
+          {name: 'Eve'}
+        ]}/>
+        <ContactList contacts={[
+          {name: 'John'},
+          {name: 'Alex'},
+          {name: 'Donna'}
+        ]}/>
+        <ContactList contacts={[
+          {name: 'Elvis'},
+          {name: 'Jack'},
+          {name: 'Adam'}
+        ]}/>
       </div>
-    );
+    )
   }
 }
 
