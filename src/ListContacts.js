f8baec349ab9a26115a57e616eedb5611398a5c8
fullyslick <default@mail.com>
2018-07-03 17:07:17 +0300

    Loop/map over the data to display it on DOM

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,22 @@
 // Import react component class tobe able to use it
 import React, {Component} from 'react';
 
-class ListContacts extends Component{
-  render(){
+class ListContacts extends Component {
+  render() {
     // Check the log to see the array that is passed
     console.log(this.props.contacts);
+
+    // map over the data passed from App.js: this.props.contacts
+    // for every contact create a <li>
+    // https://youtu.be/qkKNrTUvGJU
     return (
-      <ol className="contact-list">Order List</ol>
+      <ol className="contact-list">
+        {this.props.contacts.map(
+          (contact) => (
+            <li key={contact.id}>{contact.name}</li>
+          )
+        )}
+      </ol>
     )
   }
 }
