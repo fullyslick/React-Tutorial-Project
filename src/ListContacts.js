2d9bb90bc7caa8037c48bcdefd12059ac5ff2fd9
fullyslick <default@mail.com>
2018-07-03 17:19:33 +0300

    Add style to contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -13,7 +13,17 @@ class ListContacts extends Component {
       <ol className="contact-list">
         {this.props.contacts.map(
           (contact) => (
-            <li key={contact.id}>{contact.name}</li>
+          // For details: https://youtu.be/mnIuUk9cexA   
+            <li key={contact.id} className="contact-list-item">
+              <div className="contact-avatar" style={{ backgroundImage:`url(${contact.avatarURL})`}}></div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button className="contact-remove">
+                Remove
+              </button>
+            </li>
           )
         )}
       </ol>
