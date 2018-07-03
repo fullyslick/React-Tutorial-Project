0832c57a72efa8bb6d51b46ed3e8c4db57514e78
fullyslick <default@mail.com>
2018-07-03 16:30:34 +0300

    Move project in the recommended udacity state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     // Starting state of the app
-    return null
+    return (
+      <div>Hello World</div>
+    )
   }
 }
 
