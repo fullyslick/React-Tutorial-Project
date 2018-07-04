05ed4762309c0ad43d89b71cf5c3a1f7fd2b5119
fullyslick <default@mail.com>
2018-07-04 18:16:34 +0300

    Use class instead of function for the component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,5 @@
 // Import react component class tobe able to use it
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 // PropTypes is external npm package that allow us to
@@ -8,26 +8,33 @@ import PropTypes from 'prop-types';
 // 1. Install PropTypes: npm install --save prop-types
 // 2. Restart server if it is running.
 // 3. Import PropTypes - see above:: import PropTypes from 'prop-types';
-// 4. Replace import React, {Component} from 'react'; WITH import React, from 'react';
-// 5. Define what the props passed to the component type should be. Check at the bottom.
+// 5. Define what the props passed to the component type should be. Check inside the class ListContacts statis propTypes
 
 // If the component has only render method,
 // we can use simple function. a.k.a (Stateless Function)
 // The function will have only one argument - the property passed from App.js (customers)
 // https://youtu.be/tTxLxl_Bk3Y
-function ListContacts(props){
+class ListContacts extends Component{
+    // Here you define what the props passed to the component type should be,
+    // and wheter the property is required in order to work the ListContacts component.
+    // If these props are not passed a message will be logged in console.
+    // Now you know that the app brakes because of incorrect props passed to the component.
+    static propTypes = {
+      contacts: PropTypes.array.isRequired,
+      onDeleteContact: PropTypes.func.isRequired
+    }
+
+    render(){
     // Check the log to see the array that is passed
-    console.log(props.contacts);
+    console.log(this.props.contacts);
 
     // map over the data passed from App.js: this.props.contacts
     // for every contact create a <li>
     // https://youtu.be/qkKNrTUvGJU
     // When using statless func instead of class, just return the JSX template
     return (
-      // Replace this.props.contacts with props.contacts,
-      // there is no longer need to use "this"
       <ol className="contact-list">
-        {props.contacts.map(
+        {this.props.contacts.map(
           (contact) => (
           // For details: https://youtu.be/mnIuUk9cexA
             <li key={contact.id} className="contact-list-item">
@@ -37,7 +44,7 @@ function ListContacts(props){
                 <p>{contact.email}</p>
               </div>
               {/* Invoke removeContact function from App component passing the "conatct" that is clicked*/}
-              <button onClick={() => (props.onDeleteContact(contact))} className="contact-remove">
+              <button onClick={() => (this.props.onDeleteContact(contact))} className="contact-remove">
                 Remove
               </button>
             </li>
@@ -45,15 +52,7 @@ function ListContacts(props){
         )}
       </ol>
     )
-}
-
-// Here you define what the props passed to the component type should be,
-// and wheter the property is required in order to work the ListContacts component.
-// If these props are not passed a message will be logged in console.
-// Now you know that the app brakes because of incorrect props passed to the component.
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+  }
 }
 
 // Make the component accessable by other scripts in this case App.js
