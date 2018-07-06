933fcb1c2639cf547f37cb5671551feefb2b023c
fullyslick <default@mail.com>
2018-07-06 17:02:43 +0300

    Import BrowserRouter to be able to use it

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import broweser router to be able to use it: https://youtu.be/Re8n-hmkWlY
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root'));
+  registerServiceWorker();
