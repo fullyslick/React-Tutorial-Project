a9181c09418d7a29582aba3c185bc81c7556e951
fullyslick <default@mail.com>
2018-07-06 14:34:22 +0300

    Fetch data from server

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@
 
 import React, {Component} from 'react';
 // import the newly created ListContacts component
-import ListContacts from './ListContacts.js'
+import ListContacts from './ListContacts.js';
+// import all methods from ContactsAPI.js to be able to use them.
+// We use them to fetch data, check componentDidMount()
+import * as ContactsAPI from './utils/ContactsAPI.js';
 
 class App extends Component {
   state = {
     // The data that will be using,
-    // later it will be replaced by fetch to server localhost:5001
+    // Right now it is an "contacts" object witn an empty array.
+    // This array will be filled with data with a fetch to server localhost:5001, it holds json file.
+    // This happens in componentDidMount()
     // In order to enable React to update this data below, it should be placed inside "state" variable.
     // By having a component manage its own state, any time there are changes made to that state,
     // React will know and automatically make the necessary updates to the page.
@@ -20,28 +25,17 @@ class App extends Component {
     // It currently isn't supported by JavaScript, but thanks to Babel's fantastic powers of transpiling, we can use it!
     // Check offical docs for "state": https://reactjs.org/docs/react-without-es6.html#setting-the-initial-state
     // https://youtu.be/RyO7B5KLXVY
-    contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackso",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts : []
   }
 
+  // This method is executed automatically by React when, the component is displayed on the DOM.
+  // This is the place to make AJAX or SQL request to get some data.
+  // The request is defined in getAll() method in ContactsAPI.js NOTE check the imports at the top.
+  // After response is recieved from request, set new state to merge the data from the response with the empty array "contacts".
+  // https://youtu.be/DoDbW-DlwlI
+  componentDidMount(){
+    ContactsAPI.getAll().then((responseWithContacts) => this.setState({ contacts: responseWithContacts }));
+  }
   // Removes contact from state.contact when "x" btn us clicked in ListContacts
   // This method will be passed as new property "onDeleteContact" to <ListContacts /> below.
   // Then then ListContacts.js will be able to access it and invoke that function using onClick={ (contact) => props.onDeleteContact}
