d1d3f47947d8c7de26b33b55282cb118da2a4d67
fullyslick <default@mail.com>
2018-07-06 17:55:27 +0300

    Create image input for avatar

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+// This is compnent is provided by Udacity
+// and it allows to choose image file from local computer
+// and render it on the screen.
+// https://youtu.be/8JjYl5r3wLk?t=58
+import ImageInput from './ImageInput.js';
 
 // This component inserts new contact.
 // https://youtu.be/5ySqH5Uag2M
 class CreateContact extends Component{
   render(){
     return (
+      <div>
       <Link to="/" className="close-create-contact">Back</Link>
+      <form className="create-contact-form">
+        <ImageInput
+          className="create-contact-avatar-input"
+          name="avatrURL"
+          maxHeight={64}
+        />
+      </form>
+      </div>
     )
   }
 }
